f9417c7c43b33f1bc41f5863fce569ab83bde1ae
shuvo0074 <38061271+shuvo0074@users.noreply.github.com>
2018-04-27 11:42:56 +0600

    Edited home js, added chat js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+class Home extends React.Component{
+    render(){
+        return (
+            <View style={styles.container}>
+              
+              <Text>Changes cgnjcfjh make will automatically reload.</Text>
+              <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+          );
+    }
+}
+export default Home

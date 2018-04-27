304e946be1861c24fb98facd683a9c60fbd5d03c
shuvo0074 <38061271+shuvo0074@users.noreply.github.com>
2018-04-27 15:23:25 +0600

    added button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
 import { Router, Scene, Actions } from 'react-native-router-flux';
 class Home extends React.Component{
+    state = {
+        name: ''
+    }
     render(){
         return (
             <View style={styles.container}>
+
               <Text style={styles.title}>
               Enter your name:
               </Text>
               <TextInput
               style={styles.nameInput}
+              onChangeText={(txt)=>{
+                  this.setState({
+                    name: txt
+                  })
+              }}
+              value={this.state.name}
               />
+              <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={()=>{
+                  alert(this.state.name)
+              }} >
+                  <Text>
+                      Start chatting
+                      </Text>
+                  </TouchableOpacity>
+
             </View>
           );
     }
@@ -31,6 +51,10 @@ const styles = StyleSheet.create({
         width: 300,
         borderWidth: 1,
         margin: 10
-        
+    },
+    buttonStyle: {
+        margin: 10,
+        borderWidth: 1,
+        padding: 5,
     }
   });
